f0d04d13c34f71c4f51e7d90365e05a7a3f2a09d
John-S-Reddick <john.work.reddick@outlook.com>
2026-02-24 10:59:21 -0500

    Added Tabstops to options.lua

--- .bashrc
+++ .bashrc
@@ -1,20 +1,38 @@
 # .bashrc
-
 # Source global definitions
 if [ -f /etc/bashrc ]; then
     . /etc/bashrc
 fi
 
 # User specific environment
-if ! [[ "$PATH" =~ "$HOME/.local/bin:$HOME/bin:" ]]; then
-    PATH="$HOME/.local/bin:$HOME/bin:$PATH"
-fi
+PATH="$HOME/.local/bin:$HOME/bin:$PATH"
 export PATH
+export GOPATH="$HOME/go"
+export PATH="$PATH:$GOPATH/bin"
 
-# Uncomment the following line if you don't like systemctl's auto-paging feature:
-# export SYSTEMD_PAGER=
+# Color Manpages
+export MANROFFOPT="-c"
+export MANPAGER="less --mouse --use-color -M +Gg -Dd+y -Du+c -DE+wr -DS+km"
 
-# User specific aliases and functions
+# Alias Commands
+alias bashrc='nvim ~/.bashrc'
+alias ZZ='exit'
+alias q='exit'
+alias cls='clear'
+
+# Alias Directories
+alias cdnvim='cd ~/.config/nvim/lua'
+alias cdaoc='cd ~/Documents/advent-of-code-2025' # !! Get rid of 2026
+
+# Alias Files
+alias nvmappings='nvim ~/.config/nvim/lua/mappings.lua'
+alias nvoptions='nvim ~/.config/nvim/lua/options.lua'
+alias nvlsp='nvim ~/.config/nvim/lua/configs/lspconfig.lua'
+alias nvaf='nvim ~/.config/nvim/lua/configs/conform.lua'
+alias nvautoformat='nvim ~/.config/nvim/lua/configs/conform.lua'
+alias nvplugins='nvim ~/.config/nvim/lua/plugins/init.lua'
+
+
 if [ -d ~/.bashrc.d ]; then
     for rc in ~/.bashrc.d/*; do
         if [ -f "$rc" ]; then
@@ -23,3 +41,4 @@ if [ -d ~/.bashrc.d ]; then
     done
 fi
 unset rc
+fastfetch
--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -1,6 +1,49 @@
 require "nvchad.mappings"
 
 -- add yours here
+local function GetDiagnostic()
+  local bufnr = vim.api.nvim_get_current_buf()
+  local cursor_pos = vim.api.nvim_win_get_cursor(0)
+  local cursor_line = cursor_pos[1] - 1
+  local diagnostics = vim.diagnostic.get(bufnr, { lnum = cursor_line })
+
+  if #diagnostics == 0 then
+    vim.print "No diagnostic found"
+    return nil
+  end
+
+  vim.cmd "normal yy"
+  local diag_text = "Diagnostics:\n"
+  for _, diag in ipairs(diagnostics) do
+    diag_text = diag_text .. diag.message .. "\n"
+  end
+  local current_line = vim.fn.getreg "*"
+  current_line = current_line:sub(1, -2)
+
+  return {
+    line = current_line,
+    diagnostics = diag_text,
+  }
+end
+
+local function VFit()
+ local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+  local max_width = 0
+  for _, line in ipairs(lines) do
+    local width = vim.fn.strdisplaywidth(line)
+    if width > max_width then
+      max_width = width
+    end
+  end
+  vim.cmd("vertical resize " .. max_width + 6)
+end
+
+local function HFit()
+  local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+  local max_height = #lines
+  vim.cmd("resize " .. max_height + 1)
+end
+
 local function exec_code_run(termargs)
   pcall(function()
     vim.cmd "w"
@@ -52,19 +95,7 @@ end, { desc = "Paste pretty" })
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
-map({ "n", "v" }, "<leader>a", function()
-  require("harpoon.mark").add_file()
-end, { desc = "Harpoon Add"})
-
-map("n", "<leader>q", function()
-  require("harpoon.ui").toggle_quick_menu()
-end, { desc = "Harpoon Menu"})
-
-for i = 1, 6 do
-  map("n", "<leader>" .. i, function()
-    require("harpoon.ui").nav_file(i)
-  end)
-end
+--map("t", "<Esc>", "<C-\\><C-N>", { desc = "terminal escape terminal mode" })
 map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 map("n", "<F5>", function()
   exec_code_run "direction=float"
@@ -73,3 +104,102 @@ end, { desc = "Run Script" })
 map("n", "<S-F5>", function()
   exec_code_run "direction=vertical size=80"
 end, { desc = "Run Script" })
+
+map("n", "<Esc>", function()
+  if
+    (vim.bo.buftype == "nofile" and vim.bo.filetype == "markdown") -- Inside LSP Hover
+    or vim.bo.filetype == "oil" -- Oil
+  then
+    vim.cmd "q"
+  elseif vim.bo.buftype == "terminal" then -- NTERMINAL
+    -- Force close terminal on double-esc
+    -- First Esc to nterminal mode, second closes:
+    vim.cmd "startinsert"
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-c><C-d>", true, true, true), "i", false)
+  else
+    vim.cmd "noh"
+  end
+end)
+
+map("n", "<leader>gg", function()
+  pcall(function()
+    vim.cmd "w"
+  end)
+  vim.cmd "LazyGit"
+end, { desc = "LazyGit" })
+
+
+map("t", "<Esc>", function()
+  if vim.bo.filetype == "lazygit" then
+    vim.cmd "q"
+  else
+    vim.cmd "stopinsert"
+  end
+end, { desc = "Terminal Escape terminal mode" })
+map(
+  { "n" },
+  "<leader>t",
+  "<cmd>ToggleTerm direction=float<CR>",
+  { desc = "Terminal Toggle Floating term" }
+)
+
+map(
+  "n",
+  "<leader>v",
+  "<cmd>ToggleTerm direction=vertical<CR>",
+  { desc = "Terminal Toggle Vertical" }
+)
+map(
+  "n",
+  "<leader>h",
+  "<cmd>ToggleTerm direction=horizontal<CR>",
+  { desc = "Terminal Toggle Horizontal" }
+)
+
+-- Harpoon Mappings
+map({ "n", "v" }, "<leader>a", function()
+  require("harpoon.mark").add_file()
+end, { desc = "Harpoon Add" })
+
+map("n", "<leader>q", function()
+  require("harpoon.ui").toggle_quick_menu()
+end, { desc = "Harpoon Menu" })
+
+for i = 1, 6 do
+  map("n", "<leader>" .. i, function()
+    require("harpoon.ui").nav_file(i)
+  end)
+
+  map("n", "<leader>" .. ({ "!", "@", "#", "$", "%", "^" })[i], function()
+    -- auto determine based on aspect ratio
+    if (vim.o.columns / vim.o.lines) > 2.4 then
+      vim.cmd "vnew"
+      require("harpoon.ui").nav_file(i)
+      VFit()
+    else
+      vim.cmd "new"
+      require("harpoon.ui").nav_file(i)
+      HFit()
+    end
+  end)
+end
+map({ "n", "x" }, "<leader>sr", function() require("ssr").open() end)
+map({ "n", "v" }, "<leader>d", '"_d', { desc = "d (no copy)"})
+map("n", "<leader>lf", function()
+  vim.diagnostic.open_float(nil, { border = "rounded" })
+end, { desc = "Lsp floating diagnostics" })
+map("n", "<C-a>", "<cmd>%y+<CR>", { desc = "File Copy whole" })
+map("n", "<leader>ll", function()
+  local diag_info = GetDiagnostic()
+  if diag_info then
+    vim.fn.setreg("*", "Line: `" .. diag_info.line .. "`\n" .. diag_info.diagnostics)
+  end
+end, { desc = "Copy diagnostics" })
+
+
+map("n", "<Tab>", "V>ll", { desc = "Indent line" })
+map("n", "<S-Tab>", "V<hh", { desc = "De-indent line" })
+
+map("v", "<Tab>", ">llgv", { desc = "Indent line" })
+map("v", "<S-Tab>", "<hhgv", { desc = "De-indent line" })
+
