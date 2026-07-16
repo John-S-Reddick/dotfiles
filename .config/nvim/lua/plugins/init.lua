95af2c954a1915fea3cf4fe980cf1dd32ab11f86
John-S-Reddick <john.work.reddick@outlook.com>
2026-07-16 12:47:55 -0400

    Cleaned up some duplicate aliases

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -25,7 +25,18 @@ return {
       require("telescope").load_extension "lazygit"
     end,
   },
-
+  {
+  "lervag/vimtex",
+  lazy = false,     -- we don't want to lazy load VimTeX
+  -- tag = "v2.15", -- uncomment to pin to a specific release
+  init = function()
+    -- VimTeX configuration goes here, e.g.
+    vim.g.vimtex_view_method = "zathura"
+    vim.g.tex_flavor='latex'
+    vim.g.vimtex_view_method='zathura'
+    vim.g.tex_conceal='abdmg'
+  end
+  },
   {
     "ThePrimeagen/harpoon",
      dependencies = { "nvim-lua/plenary.nvim" }
